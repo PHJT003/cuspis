c98ad9e5d19cc8e3293ca24280944227be3ac78a
Valerio Villani <Valerio.Villani.2019@live.rhul.ac.uk>
2023-04-28 16:25:59 +0100

    Fix: allow the user to suppress plotting

--- getEcgThresh.m
+++ getEcgThresh.m
@@ -4,7 +4,7 @@ arguments
     MpSys(1,1) struct;
     pctRWave(1,1) {mustBeInRange(pctRWave, 0, 1)} = 0.6;
     hr(1,1) {mustBeInteger, mustBePositive} = 97; % doi.org/10/b6kkjh
-    secsToPlot(1,1) {mustBeInteger, mustBePositive, mustBeGreaterThanOrEqual(secsToPlot, 1)} = 5;
+    secsToPlot(1,1) {mustBeInteger, mustBeGreaterThanOrEqual(secsToPlot, 0)} = 5;
 end
 %% DESCRIPTION
 
@@ -19,22 +19,24 @@ end
 thresh = mean(pks)*pctRWave;
 
 %% PLOT RESULTS
-x = 1:length(ecg);
-y = ecg;
-txt = sprintf('Threshold = %.2f', thresh);
-nDp = MpSys.fs*secsToPlot;
-dpIdx = randsample(max(x)-nDp, 1);
-epoch = dpIdx:dpIdx+nDp-1;
-
-figure(101);
-plot(x, y, x(locs), pks, 'o');
-yline(thresh,'r-', txt, 'LineWidth', 1);
-title('Detected R-waves');
-                                      
-figure(102)
-plot(x(epoch), y(epoch));
-yline(thresh,'r-',txt, 'LineWidth', 1);
-title('ECG - Random epoch');
-subtitle(strcat('(', num2str(secsToPlot), ' seconds)'));
+if secsToPlot > 0
+    x = 1:length(ecg);
+    y = ecg;
+    txt = sprintf('Threshold = %.2f', thresh);
+    nDp = MpSys.fs*secsToPlot;
+    dpIdx = randsample(max(x)-nDp, 1);
+    epoch = dpIdx:dpIdx+nDp-1;
+    
+    figure(101);
+    plot(x, y, x(locs), pks, 'o');
+    yline(thresh,'r-', txt, 'LineWidth', 1);
+    title('Detected R-waves');
+    
+    figure(102)
+    plot(x(epoch), y(epoch));
+    yline(thresh,'r-',txt, 'LineWidth', 1);
+    title('ECG - Random epoch');
+    subtitle(strcat('(', num2str(secsToPlot), ' seconds)'));
+end
 
 end
