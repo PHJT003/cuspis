61f00e58dcd306732b5d64d6ec0da44de78b8cce
Valerio Villani <Valerio.Villani.2019@live.rhul.ac.uk>
2023-05-02 11:24:52 +0100

    Refactor: rename function and its arguments

--- prepStimStruct.m
+++ prepStimStruct.m
@@ -1,6 +1,6 @@
-function Img = prepImgStruct(imagePath, soaSecs, durSecs, windowPtr)
+function Stim = prepStimStruct(stimPath, soaSecs, durSecs, windowPtr)
 arguments
-    imagePath(1,:) {mustBeText, mustBeFile};
+    stimPath(1,:) {mustBeText, mustBeFile};
     soaSecs(1,1) {mustBeNumeric, mustBeGreaterThanOrEqual(soaSecs, 0)} = 0.0;
     durSecs(1,1) {mustBeNumeric, mustBeGreaterThanOrEqual(durSecs, 1e-3)} = 0.200;
     windowPtr(1,1) {mustBeNumeric, mustBeGreaterThanOrEqual(windowPtr, 0)} = max(Screen('Screens'));
@@ -9,9 +9,9 @@ end
 %% DESCRIPTION
 
 %% SET PARAMETERS
-Img.loc = imagePath;
-Img.soa = soaSecs;
-Img.dur = durSecs;
-Img.windowPtr = windowPtr;
+Stim.loc = stimPath;
+Stim.soa = soaSecs;
+Stim.dur = durSecs;
+Stim.windowPtr = windowPtr;
 
 end
