33134544ab637f69d07aec0c2101771e6585cd24
Valerio Villani <Valerio.Villani.2019@live.rhul.ac.uk>
2023-05-02 17:50:08 +0100

    Add: nPres argument
    Number of times the stimulus is presented.

--- prepStimStruct.m
+++ prepStimStruct.m
@@ -1,9 +1,10 @@
-function Stim = prepStimStruct(stimPath, soaSecs, durSecs, windowPtr)
+function Stim = prepStimStruct(stimPath, soaSecs, durSecs, windowPtr, nPres)
 arguments
     stimPath(1,:) {mustBeText, mustBeFile};
     soaSecs(1,1) {mustBeNumeric, mustBeGreaterThanOrEqual(soaSecs, 0)} = 0.0;
     durSecs(1,1) {mustBeNumeric, mustBeGreaterThanOrEqual(durSecs, 1e-3)} = 0.200;
     windowPtr(1,1) {mustBeNumeric, mustBeGreaterThanOrEqual(windowPtr, 0)} = max(Screen('Screens'));
+    nPres(1,1) {mustBeInteger, mustBePositive} = 1;
 end
 
 %% DESCRIPTION
@@ -13,5 +14,6 @@ Stim.loc = stimPath;
 Stim.soa = soaSecs;
 Stim.dur = durSecs;
 Stim.windowPtr = windowPtr;
+Stim.nPres = nPres;
 
 end
