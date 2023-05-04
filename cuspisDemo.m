80834b7d63434e6e475caa15bac52dd120fcf6c5
Valerio Villani <Valerio.Villani.2019@live.rhul.ac.uk>
2023-05-04 18:29:42 +0100

    Fix: demo.
    Now it works.

--- cuspisDemo.m
+++ cuspisDemo.m
@@ -9,25 +9,28 @@ else
     nPres = 3;
 end
 
+Bhapi = setApi();
+MpSys = setDaq();
+openApi(Bhapi, MpSys);
+ecg = recSignal(Bhapi, MpSys, 6, 'seconds', 0.5);
+thresh = getEcgThresh(ecg, MpSys, 0.6, 97, 0);
 commandwindow();
 
 Screen('Preference', 'SkipSyncTests', 1);
 PsychDefaultSetup(2);
-
 screenNo = max(Screen('Screens'));
 [windowPtr, windowRect] = Screen('OpenWindow', screenNo, [150 150 150]);
-
 Screen('BlendFunction', windowPtr, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
 
 %% PREPARE STIMULUS
-
 Stim = prepStim(fullfile(pwd(), 'circle.jpg'), windowPtr, 0.100, 0.300, nPres);
 
 %% TRIGGER TRIAL
 [D, signal] = triggerTrial(Stim, thresh, 6, Bhapi, MpSys, 0.05);
 
-%% 
-WaitSecs(2);
+%%
+closeApi(Bhapi, MpSys);
+WaitSecs(0.5);
 ShowCursor();
 sca;
 
