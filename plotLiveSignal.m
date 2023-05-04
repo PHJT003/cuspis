2d5354f92816f27f3ad2ba22af297ad01e8e79c8
Valerio Villani <Valerio.Villani.2019@live.rhul.ac.uk>
2023-05-04 12:49:50 +0100

    Docs: add help and description

--- plotLiveSignal.m
+++ plotLiveSignal.m
@@ -7,7 +7,44 @@ arguments
     unit char {mustBeMember(unit, {'minutes', 'seconds'})} = 'seconds';
     slideWinPct(1,1) {mustBeInRange(slideWinPct, 0, 1)} = 0.05;
 end
-%% DESCRIPTION
+% PLOTLIVESIGNAL Records and plots the signal in real-time.
+%   [signal, hr] = PLOTLIVESIGNAL(Bhapi, MpSys) records an ECG for 12
+%   seconds, using a sliding window whose size is 5% of the sampling
+%   frequency. The heart rate is estimated too.
+%
+%   signal = PLOTLIVESIGNAL(Bhapi, MpSys, 0, 5, 'minutes', 0.20) records a
+%   signal for 5 minutes, using a sliding window whose size is 20% of the
+%   sampling frequency.
+%
+%   See also RECSIGNAL, GETECGTHRESH.
+%
+%
+%
+% === DESCRIPTION =========================================================
+% This function is used to record and plot a signal in real-time.
+% If the signal is an ECG, you can estimate the heart rate by setting:
+% isEcg = true, t = 12, and unit = 'seconds'. The heart rate is estimated
+% with the getEcgThresh() function (default argument values).
+%
+% INPUT
+% - Bhapi        API settings.
+% - MpSys        DAQ settings.
+% - isEcg        If the signal is an ECG.
+% - t            Time to record for. The default is 12.
+% - unit         Unit of time. The default is 'seconds'.
+% - slideWinPct  The size of the sliding window, expressed as a percentage
+%                of the sampling frequency. If MpSys.fs is 100 Hz and
+%                slideWinPct is set to 0.05, then the signal will be
+%                acquired in chunks of 5 data-points.
+% OUTPUT
+% - signal       Numeric vector with the recorded signal.
+% - hr           Numeric value expressing the heart rate in BPM.
+%
+% ----------
+% Author : Valerio Villani
+% E-mail : valerio.cn2@gmail.com
+% Created: 2023-04-28, using MATLAB 9.10.0.1669831 (R2021a) Update 2
+% =========================================================================
 
 %% SET PARAMETERS
 if strcmp(unit, 'seconds')
@@ -43,14 +80,17 @@ end
 
 signal = nan(1, dpToStore);
 while(dpToStore > 0)
+    % record signal
     [MpSys.status, dpBuffer, dpStored] = calllib(Bhapi.lib, 'receiveMPData', dpBuffer, sws, dpStored);
     if ~strcmp(MpSys.status,'MPSUCCESS')
         fprintf('FAILED to receive MP data!\n');
         calllib(Bhapi.lib, 'disconnectMPDev');
         return
     else
+        % save signal
         signal(dpOffset:dpOffset+dpStored-1) = dpBuffer(1:dpStored);
         
+        % plot signal in real-time
         len = length(~isnan(signal));
         y = signal(1:len);
         x(1:len) = (1:len);
@@ -60,7 +100,6 @@ while(dpToStore > 0)
         
         dpOffset = dpOffset + sws;
         dpToStore = dpToStore - sws;
-        %         fprintf('Progress: %.2f%%\n', 100-((dpToStore/(MpSys.fs*recSecs))*100));
     end
 end
 
