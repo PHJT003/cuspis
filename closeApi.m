0069eb693416340dd44151dfceb61028a3643f2f
Valerio Villani <Valerio.Villani.2019@live.rhul.ac.uk>
2023-05-04 10:10:51 +0100

    Docs: add help and description

--- closeApi.m
+++ closeApi.m
@@ -3,7 +3,27 @@ arguments
     Bhapi(1,1) struct;
     MpSys(1,1) struct;
 end
-%% DESCRIPTION
+% CLOSEAPI Ends the application programming interface.
+%   CLOSEAPI(Bhapi, MpSys) ends the BHAPI with the settings specified in
+%   the Bhapi and MpSys structure arrays.
+%
+%   See also OPENAPI, SETAPI, SETDAQ.
+%
+%
+%
+% === DESCRIPTION =========================================================
+% This function switches off the BIOPAC Hardware Application Programming
+% Interface (BHAPI). Before recording, openApi() is used.
+%
+% INPUT
+% - Bhapi       API settings.
+% - MpSys       DAQ settings.
+%
+% ----------
+% Author : Valerio Villani
+% E-mail : valerio.cn2@gmail.com
+% Created: 2023-04-26, using MATLAB 9.10.0.1669831 (R2021a) Update 2
+% =========================================================================
 
 %% SHUT DOWN
 fprintf('\nDisconnecting...\n')
